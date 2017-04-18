dd882331b46ff3b2abf00f74b0b58cd8a0f100eb
JocoolQ <jocool.lm@gmail.com>
2017-04-18 10:36:08 -0500

    Add files via upload

--- Ejercicio Expresiones Lambda.py
+++ Ejercicio Expresiones Lambda.py
@@ -1,23 +1,34 @@
 from functools import reduce
-c = [[39, 36, 37, 38, 37,True,False,False,True],[49, 36, 37, 38, 37,True,False,False,True]]
+def infoMatriz(matriz):
+    
+    apuestas= [list(filter(lambda i: i!=True and i!=False, i)) for i in matriz]
+    gananciasBool= [list(filter(lambda i: i==True or i==False, i)) for i in matriz]
+    promedio= [ reduce(lambda x,y: x+y, i)/len(i) for i in apuestas]
+    promedioTotal= reduce(lambda x,y: x+y, promedio)/len(promedio) 
 
-apuestas= [list(filter(lambda i: i!=True and i!=False, i)) for i in c]
-gananciasBool= [list(filter(lambda i: i==True or i==False, i)) for i in c]
-promedio= [ reduce(lambda x,y: x+y, i)/len(i) for i in apuestas]
-promedioTotal= reduce(lambda x,y: x+y, promedio)/len(promedio) 
+    g = list( map(lambda i,j: list(map(lambda a,b: a if (b) else 0, i, j)), apuestas, gananciasBool))
+    ganancias= [reduce(lambda x,y: x+y, i) for i in g]
 
-g = list( map(lambda i,j: list(map(lambda a,b: a if (b) else 0, i, j)), apuestas, gananciasBool))
-ganancias= [reduce(lambda x,y: x+y, i) for i in g]
+    mayorGanancia= max(ganancias)
 
-p=list( map(lambda i,j: list(map(lambda a,b: a if (not b) else 0, i, j)), apuestas, gananciasBool))
-perdidas= [reduce(lambda x,y: x+y, i) for i in p]
+    p=list( map(lambda i,j: list(map(lambda a,b: a if (not b) else 0, i, j)), apuestas, gananciasBool))
+    perdidas= [reduce(lambda x,y: x+y, i) for i in p]
 
+    mayorPerdida= max(perdidas)
 
-print("El promedio por jugador es:")
-print(promedio)
-print("El promedio total es:")
-print(promedioTotal)
-print("Las ganancias por jugador son:")
-print(ganancias)
-print("Las pérdidas por jugador son:")
-print(perdidas)
+    print("El promedio por jugador es:")
+    print(promedio)
+    print("El promedio total es:")
+    print(promedioTotal)
+    print("Las ganancias por jugador son:")
+    print(ganancias)
+    print("Las pérdidas por jugador son:")
+    print(perdidas)
+    print("La mayor ganancia fue de:")
+    print(mayorGanancia)
+    print("La mayor pérdida fue de:")
+    print(mayorPerdida)
+
+matriz= [[39, 36, 37, 38, 37,True,False,False,True],[49, 36, 37, 38, 37,True,False,False,True]]
+
+infoMatriz(matriz)
